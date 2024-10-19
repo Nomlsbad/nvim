5414a0504702d33361fd0e60bb3b023466462c3c
Yurasov Ilia <iliayurasov@yandex.ru>
2024-10-19 03:22:20 +0300

    change maxlen to 100

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,22 +1,22 @@
 local options = {
-    formatters_by_ft = {
-        lua = { "stylua" },
-        go = { "gofumpt", "goimports", "golines" },
-        -- css = { "prettier" },
-        -- html = { "prettier" },
-    },
+  formatters_by_ft = {
+    lua = { "stylua" },
+    go = { "gofumpt", "goimports", "golines" },
+    -- css = { "prettier" },
+    -- html = { "prettier" },
+  },
 
-    formatters = {
-        golines = {
-            prepend_args = { "--max-len=80" },
-        },
+  formatters = {
+    golines = {
+      prepend_args = { "--max-len=100" },
     },
+  },
 
-    format_on_save = {
-        -- These options will be passed to conform.format()
-        timeout_ms = 500,
-        lsp_fallback = true,
-    },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 return options
