c8e082319a6336c12d23f69b2d7a60dc24c2fb50
Ilia Yurasov <iliayurasov@yandex.ru>
2025-08-21 01:28:28 +0300

    add ident line plugin

---
+++ lua/plugins/snaks/ident.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "folke/snacks.nvim",
+    ---@type snacks.Config
+    opts = {
+      indent = {
+        enabled = true,
+        animate = {
+          duration = {
+            total = 200,
+          },
+        },
+      },
+    },
+  },
+}
