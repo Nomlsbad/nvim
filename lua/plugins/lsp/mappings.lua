5a56ca24506c617e80748022781d9ec9afafaf10
Ilia Yurasov <iliayurasov@yandex.ru>
2025-08-24 19:42:46 +0300

    update mappings for lsp

--- lua/plugins/lsp/mappings.lua
+++ lua/plugins/lsp/mappings.lua
@@ -14,9 +14,23 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
     map("n", "gD", vim.lsp.buf.declaration, { desc = "Go to Declaration" })
     map("n", "gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
-    map("n", "K", vim.lsp.buf.hover, { desc = "Hover" })
+    map("n", "gY", vim.lsp.buf.type_definition, { desc = "Go to Type Definition" })
     map("n", "gi", vim.lsp.buf.implementation, { desc = "Go to Implementation" })
+    map("n", "gr", vim.lsp.buf.references, { desc = "References" })
+    map("n", "gh", function()
+      vim.lsp.buf.typehierarchy("subtypes")
+    end, { desc = "Subtypes Hierarchy" })
+    map("n", "gH", function()
+      vim.lsp.buf.typehierarchy("supertypes")
+    end, { desc = "Supertypes Hierarchy" })
+
+    map({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
+    map("n", "<space>cr", vim.lsp.buf.rename, { desc = "Rename" })
+
+    map("n", "K", vim.lsp.buf.hover, { desc = "Hover" })
     map("n", "<C-k>", vim.lsp.buf.signature_help, { desc = "Signature Help" })
+    map("n", "D", vim.diagnostic.open_float, { desc = "Diagnostic" })
+
     map("n", "<space>wa", vim.lsp.buf.add_workspace_folder, { desc = "Add Workspace Folder" })
     map(
       "n",
@@ -30,9 +44,5 @@ vim.api.nvim_create_autocmd("LspAttach", {
       function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end,
       { desc = "List Workspace Folder" }
     )
-    map("n", "gY", vim.lsp.buf.type_definition, { desc = "Go to Type Definition" })
-    map("n", "<space>rn", vim.lsp.buf.rename, { desc = "Rename" })
-    map({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, { desc = "Code Action" })
-    map("n", "gr", vim.lsp.buf.references, { desc = "References" })
   end,
 })
